8cebe247cdc3a42ab5ec06545f31b8b0e4863b27
Julian Goh <juliangoh5@gmail.com>
2022-02-04 12:17:16 +0800

    4/2/22 - Relocated command.sh

---
+++ command.sh
@@ -0,0 +1,13 @@
+# REPO_URL <https://github.com/JulianGoh/Forage-Blackbird-ReactApp>
+
+npx create-react-app my-app
+cd my-app
+npm start
+In line 8 of App.js, replaced src with new link to Propeller image
+In line 14 of App.js, replaced href with new Propeller link
+
+
+
+
+
+
